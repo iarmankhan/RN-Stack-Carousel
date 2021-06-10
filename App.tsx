a17e5b2a7a59f31318d5ab34de87a6d2f7a19ddb
iarmankhan <work.armankhan@gmail.com>
2021-06-10 22:00:31 +0530

    :construction: header setup, added data

--- App.tsx
+++ App.tsx
@@ -1,18 +1,38 @@
 import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {Animated, Dimensions, SafeAreaView, StyleSheet} from 'react-native';
+import OverflowItems from "./OverflowItems";
+import {DATA} from "./data";
+
+
+const {width} = Dimensions.get('window')
+
+const ITEM_WIDTH = width * 0.76;
+const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
+const VISIBLE_ITEMS = 3;
+
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar hidden />
-    </SafeAreaView>
-  );
+    const scrollXIndex = useRef(new Animated.Value(0)).current;
+    const scrollXAnimated = useRef(new Animated.Value(0)).current;
+    const [index, setIndex] = useState(0);
+
+    const setActiveIndex = React.useCallback((activeIndex) => {
+        scrollXIndex.setValue(activeIndex);
+        setIndex(activeIndex);
+    }, []);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <StatusBar hidden/>
+            <OverflowItems data={DATA} scrollX={scrollXAnimated}/>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
---
+++ OverflowItems.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Animated, StyleSheet, Text, View} from 'react-native';
+import {ITEM} from "./data";
+import {EvilIcons} from "@expo/vector-icons";
+
+interface OverflowItemsProps {
+    data: ITEM[];
+    scrollX: Animated.Value
+}
+
+const OVERFLOW_HEIGHT = 80
+const SPACING = 10
+
+const OverflowItems: React.FC<OverflowItemsProps> = ({data, scrollX}) => {
+    return <View style={styles.container}>
+        <Animated.View>
+            {
+                data.map((item, index) => (
+                    <View key={index} style={styles.itemContainer}>
+                        <Text style={[styles.title]} numberOfLines={1}>
+                            {item.title}
+                        </Text>
+                        <View style={styles.itemContainerRow}>
+                            <Text style={[styles.location]}>
+                                <EvilIcons
+                                    name='location'
+                                    size={16}
+                                    color='black'
+                                    style={{marginRight: 5}}
+                                />
+                                {item.location}
+                            </Text>
+                            <Text style={[styles.date]}>{item.date}</Text>
+                        </View>
+                    </View>
+                ))
+            }
+        </Animated.View>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+    container: {
+        height: OVERFLOW_HEIGHT,
+        overflow: 'hidden',
+    },
+    itemContainer: {
+        height: OVERFLOW_HEIGHT,
+        padding: SPACING * 2,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: '900',
+        textTransform: 'uppercase',
+        letterSpacing: -1,
+        marginBottom: 8
+    },
+    itemContainerRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    location: {
+        fontSize: 16
+    },
+    date: {
+        fontSize: 12
+    }
+});
+
+export default OverflowItems;
