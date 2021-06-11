7a46f49856012cb181417f9b7591ec483d04b346
iarmankhan <work.armankhan@gmail.com>
2021-06-11 23:17:31 +0530

    :construction: added navigation for shared element transition

--- App.tsx
+++ App.tsx
@@ -1,141 +1,25 @@
 import 'react-native-gesture-handler';
-import {StatusBar} from 'expo-status-bar';
-import React, {useRef, useState} from 'react';
-import {Animated, Dimensions, FlatList, Image, SafeAreaView, StyleSheet, View} from 'react-native';
-import OverflowItems, {SPACING} from "./OverflowItems";
-import {DATA, ITEM} from "./data";
-import {FlingGestureHandler, State, Directions} from "react-native-gesture-handler";
+import React from 'react';
+import {Dimensions, StyleSheet} from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createSharedElementStackNavigator} from "react-navigation-shared-element";
+import Events from "./Events";
+import EventDetails from "./EventDetails";
 
 
-const {width} = Dimensions.get('window')
-
-const ITEM_WIDTH = width * 0.76;
-const ITEM_HEIGHT = ITEM_WIDTH * 1.7;
-const VISIBLE_ITEMS = 3;
-
+const Stack = createSharedElementStackNavigator();
 
 export default function App() {
-    const [data, setData] = useState<ITEM[]>(DATA)
-    const scrollXIndex = useRef(new Animated.Value(0)).current;
-    const scrollXAnimated = useRef(new Animated.Value(0)).current;
-    const [index, setIndex] = useState(0);
-
-    const setActiveIndex = React.useCallback((activeIndex) => {
-        scrollXIndex.setValue(activeIndex);
-        setIndex(activeIndex);
-    }, []);
-
-    React.useEffect(() => {
-        Animated.spring(scrollXAnimated, {
-            toValue: scrollXIndex,
-            useNativeDriver: true,
-        }).start();
-    });
-
-
     return (
-        <FlingGestureHandler
-            key='left'
-            direction={Directions.LEFT}
-            onHandlerStateChange={e => {
-                if (e.nativeEvent.state === State.END) {
-
-                    if (index === data.length - 1) {
-                        return;
-                    }
-                    setActiveIndex(index + 1)
-                }
-            }}
-        >
-            <FlingGestureHandler
-                key='right'
-                direction={Directions.RIGHT}
-                onHandlerStateChange={e => {
-                    if (e.nativeEvent.state === State.END) {
-                        if (index === 0) {
-                            return;
-                        }
-                        setActiveIndex(index - 1)
-                    }
-                }}
-            >
-                <SafeAreaView style={styles.container}>
-                    <StatusBar hidden/>
-                    <OverflowItems data={data} scrollX={scrollXAnimated}/>
-
-                    <FlatList
-                        horizontal
-                        inverted
-                        contentContainerStyle={{
-                            flex: 1,
-                            justifyContent: 'center',
-                            padding: SPACING * 2,
-                            marginTop: 50,
-                        }}
-                        scrollEnabled={false}
-                        removeClippedSubviews={false}
-                        showsHorizontalScrollIndicator={false}
-                        data={data}
-                        keyExtractor={(_, index) => String(index)}
-                        CellRendererComponent={({
-                                                    item,
-                                                    index,
-                                                    children,
-                                                    style,
-                                                    ...props
-                                                }) => {
-                            const newStyle = [style, {zIndex: data.length - index}];
-
-                            return (
-                                <View style={newStyle} index={index} {...props}>
-                                    {children}
-                                </View>
-                            )
-                        }}
-                        renderItem={({item, index}) => {
-                            const inputRange = [index - 1, index, index + 1]
-                            const translateX = scrollXAnimated.interpolate({
-                                inputRange,
-                                outputRange: [50, 0, -100]
-                            })
-                            const scale = scrollXAnimated.interpolate({
-                                inputRange,
-                                outputRange: [0.8, 1, 1.3],
-                            });
-                            const opacity = scrollXAnimated.interpolate({
-                                inputRange,
-                                outputRange: [1 - 1 / VISIBLE_ITEMS, 1, 0],
-                            });
-                            return (
-                                <Animated.View
-                                    style={{
-                                        position: 'absolute',
-                                        left: -ITEM_WIDTH / 2,
-                                        opacity,
-                                        transform: [{translateX}, {scale}]
-                                    }}
-                                >
-                                    <Image
-                                        source={{uri: item.poster}}
-                                        style={{
-                                            width: ITEM_WIDTH,
-                                            height: ITEM_HEIGHT,
-                                            borderRadius: 14,
-                                        }}
-                                    />
-                                </Animated.View>
-                            )
-                        }}
-                    />
-                </SafeAreaView>
-            </FlingGestureHandler>
-        </FlingGestureHandler>
-    );
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Events" headerMode='none'>
+                <Stack.Screen name="Events" component={Events} />
+                <Stack.Screen
+                    name="EventDetails"
+                    component={EventDetails}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-});
---
+++ EventDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native';
+
+interface EventDetailsProps {
+}
+
+const EventDetails: React.FC<EventDetailsProps> = () => {
+    return <View />;
+};
+
+
+export default EventDetails;
